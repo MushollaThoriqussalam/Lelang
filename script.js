42c9828075f6dac381310835bfa6b6759de5d056
musholahThariqussalaam <derispunyaa@gmail.com>
2021-09-18 15:08:32 +0800

    perbaikan

--- script.js
+++ script.js
@@ -2,15 +2,10 @@ let app = new function(){
   let harga1 = 100000000
   let hargaNinja = document.querySelector('.hargaNinja')
   hargaNinja.innerHTML = "Rp " + harga1.toLocaleString()
+  let kelipatan = 500000
 
   let nama = []
   let buttonBid = document.querySelector('#bid1')
-
-  buttonBid.addEventListener('click', function(){
-    if (nama === '') {
-      console.log('nama kosong');
-    }
-  })
 
   this.el = document.querySelector(".bidding")
   this.bidding = []
@@ -18,17 +13,135 @@ let app = new function(){
   this.FetchAll = function() {
     let data = ""
 
-    if (this.bidding.length > 0) {
+    // if (nama.length === 0) {
+    //   data += '<div class="alert alert-danger alert-dismissible fade show" role="alert">'
+    //   data += 'Anda belum input nama!</div>'
+    //   data += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button'
+    // }
+
+    if (this.bidding.length > 0 && this.bidding.length < 4) {
       let belumAda = document.querySelector('.belumAda')
       belumAda.style.display = 'none'
 
-      for (let i = 0; i < this.bidding.length; i++) {
+      let tawar = document.querySelector('.tawar')
+      tawar.innerHTML = 'Bid Selanjutnya'
+
+        let hargaSelanjutnya = this.bidding[0] + kelipatan
+        hargaNinja.innerHTML = hargaSelanjutnya.toLocaleString()
+        
+      
+
+      if (this.bidding.length === 2) {
+        console.log('2 jalan');
+        let temp1 = this.bidding[0]
+        this.bidding[0] = this.bidding[1]
+        this.bidding[1] = temp1
+
+        // Nama
+        let tempNama = nama[0]
+        nama[0] = nama[1]
+        nama[1] = tempNama
+
+        for (let i = 0; i < this.bidding.length; i++) {
+          console.log(nama[i]);
+          data += '<div class="row text-center">'
+          data += '<div class="col">'
+          data += '<p class="nama">' + nama[i].toUpperCase() + '</p>'
+          if (i === 0) {
+            data += '<span class="badge bg-success">Rp. ' + this.bidding[i].toLocaleString() +'</span></div></div>' 
+          } else {
+            data += '<span class="badge bg-dark">Rp. ' + this.bidding[i].toLocaleString() +'</span></div></div>' 
+          }
+        }
+      } else if (this.bidding.length === 3){
+        console.log('3 jalan');
+        let temp1 = this.bidding[0]
+        this.bidding[0] = this.bidding[2]
+        this.bidding[2] = temp1
+
+        let temp2 = this.bidding[1]
+        this.bidding[1] = this.bidding[2]
+        this.bidding[2] = temp2
+
+        // Nama
+        let tempNama1 = nama[0]
+        nama[0] = nama[2]
+        nama[2] = tempNama1
+
+        let tempNama = nama[1]
+        nama[1] = nama[2]
+        nama[2] = tempNama
+
+        for (let i = 0; i < this.bidding.length; i++) {
 
           data += '<div class="row text-center">'
           data += '<div class="col">'
-          data += '<p class="nama">' + nama[i] + '</p>'
+          data += '<p class="nama">' + nama[i].toUpperCase() + '</p>'
+          if (i === 0) {
+            data += '<span class="badge bg-success">Rp. ' + this.bidding[i].toLocaleString() +'</span></div></div>' 
+          } else {
+            data += '<span class="badge bg-dark">Rp. ' + this.bidding[i].toLocaleString() +'</span></div></div>' 
+          }
+        }
+      } else {
+        for (let i = 0; i < this.bidding.length; i++) {
+
+          data += '<div class="row text-center">'
+          data += '<div class="col">'
+          data += '<p class="nama">' + nama[i].toUpperCase() + '</p>'
           data += '<span class="badge bg-success">Rp. ' + this.bidding[i].toLocaleString() +'</span></div></div>' 
+        }
       }
+      
+    } else if (this.bidding.length === 4){
+        let belumAda = document.querySelector('.belumAda')
+        belumAda.style.display = 'none'
+
+        let tawar = document.querySelector('.tawar')
+        tawar.innerHTML = 'Bid Selanjutnya'
+        console.log('4 jalan');
+
+        let temp1 = this.bidding[2]
+        this.bidding[2] = this.bidding[3]
+        this.bidding[3] = temp1
+
+        let temp2 = this.bidding[0]
+        this.bidding[0] = this.bidding[2]
+        this.bidding[2] = temp2
+
+        let temp3 = this.bidding[1]
+        this.bidding[1] = this.bidding[2]
+        this.bidding[2] = temp3
+
+        this.bidding.pop()
+
+        // Nama
+        let tempNama2 = nama[2]
+        nama[2] = nama[3]
+        nama[3] = tempNama2
+
+        let tempNama3 = nama[0]
+        nama[0] = nama[2]
+        nama[2] = tempNama3
+
+        let tempNama4 = nama[1]
+        nama[1] = nama[2]
+        nama[2] = tempNama4
+
+        nama.pop()
+
+        for (let i = 0; i < this.bidding.length; i++) {
+
+          data += '<div class="row text-center">'
+          data += '<div class="col">'
+          data += '<p class="nama">' + nama[i].toUpperCase() + '</p>'
+          if (i === 0) {
+            data += '<span class="badge bg-success">Rp. ' + this.bidding[i].toLocaleString() +'</span></div></div>' 
+          } else {
+            data += '<span class="badge bg-dark">Rp. ' + this.bidding[i].toLocaleString() +'</span></div></div>' 
+          }
+        }
+      
     }
 
     return this.el.innerHTML = data;
@@ -62,11 +175,15 @@ let app = new function(){
 
     let result = ""
     for (let i = 0; i < input1.length; i++) {
-      if (input1[i] !== '.' || input1[i] !== ',') {
+      if (input1[i] !== '.') {
         result += input1[i]
       }
     }
+    console.log(result);
+    result = Number(result)
     
+    
+
     if (result) {
       harga1 = result
       this.bidding.push(result)
